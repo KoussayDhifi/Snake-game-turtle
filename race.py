704e5ce1d1ec85a281fbf74fcf803b6f34e72621
Koussay <koussaydhifi48@gmail.com>
2021-10-30 21:56:20 +0100

    Make telePORTU

--- race.py
+++ race.py
@@ -31,5 +31,21 @@ while snake:
     t.onkey(down,"Down")
     t.onkey(left,"Left")
     t.onkey(right,"Right")
+    if (t.xcor() == 500):
+        t.hideturtle()
+        t.goto(-500,t.ycor())
+        t.showturtle()
+    elif (t.xcor() == -500):
+        t.hideturtle()
+        t.goto(500,t.ycor())
+        t.showturtle()
+    elif (t.ycor()== 500):
+        t.hideturtle()
+        t.goto(t.xcor(),-450)
+        t.showturtle()
+    elif (t.ycor()== -500):
+        t.hideturtle()
+        t.goto (t.xcor(),450)
+        t.showturtle()
 
 t.mainloop()
